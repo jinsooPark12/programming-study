cdbc3618175940b3b40c64ab9e0ca46acf2f6304
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-04-03 21:20:26 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,12 +1,12 @@
 def solution(targets):
     answer = 0
-    T = 0                # 요격의 제한범위
+    T = 0                       # 요격의 제한범위
     for s, e in sorted(targets):
         # s가 제한범위 내에 있다면
         if T > s:
-            T = min(T,e)
+            T = min(T, e)
         # s가 제한범위 밖에 있다면
         else:
             answer += 1
-            T = e-0.5
+            T = e-0.5           # 요격한 미사일의 끝 범위인 e로 제한범위 변경, 실수를 고려해서 0.5를 뺌
     return answer
