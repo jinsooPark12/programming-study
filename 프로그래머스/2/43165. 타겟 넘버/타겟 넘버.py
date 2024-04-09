13915c77ab6a00d660ad403d7398783007504b78
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-04-09 22:20:51 +0900

    [level 2] Title: 타겟 넘버, Time: 2.87 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,14 +1,16 @@
+# 계산되는 합의 결과를 모두 탐색
+# 음수와 양수의 경우를 모두 따져봐야하기 때문에 dfs 알고리즘 사용
+def dfs(ans, n, numbers, target):
+        global answer
+        if n == len(numbers):
+            if ans == target:
+                answer += 1
+            return
+        dfs(ans + numbers[n], n + 1, numbers, target)
+        dfs(ans - numbers[n], n + 1, numbers, target)
+
 def solution(numbers, target):
     global answer
     answer = 0
-    def dfs(number, n):
-        global answer
-        if n == len(numbers):
-            if number == target:
-                answer += 1
-                return
-            return
-        dfs(number + numbers[n], n + 1)
-        dfs(number - numbers[n], n + 1)
-    dfs(0, 0)
+    dfs(0,0, numbers, target)
     return answer
