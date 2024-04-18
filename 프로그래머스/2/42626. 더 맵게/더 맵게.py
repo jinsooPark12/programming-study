e6a370530083cec40c77fd976f58e9dd6f5207b4
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-04-18 13:22:38 +0900

    [level 2] Title: 더 맵게, Time: 1900.18 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,12 +1,12 @@
 import heapq
 def solution(scoville, K):
-    heapq.heapify(scoville)
+    heapq.heapify(scoville)                 # 리스트를 최소 heap으로 변환
     answer = 0
     while 1:
         num1 = heapq.heappop(scoville)
-        if num1 >= K:
+        if num1 >= K:                       # 제일 맵지 않은 음식이 K보다 크면 answer 반환
             return answer
-        if len(scoville) == 0:
+        if len(scoville) == 0:              # 음식이 없으면 -1 반환
             return -1
         else:
             num2 = heapq.heappop(scoville)
