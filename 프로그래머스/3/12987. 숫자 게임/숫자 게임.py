154b4599e482980de6705ba9da371ca8d4675ace
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-05-01 18:22:51 +0900

    [level 3] Title: 숫자 게임, Time: 45.31 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -2,13 +2,11 @@ def solution(A, B):
     answer = 0
     A.sort()
     B.sort()
+    j = 0
     if min(A) > max(B):
         return 0
-    while len(A) != 0:
-        num1 = A.pop(0)
-        for i in range(len(B)):
-            num2 = B.pop(0)
-            if num2 > num1:
-                answer += 1
-                break
+    for i in range(len(A)):
+        if A[j] < B[i]:
+            answer += 1
+            j += 1
     return answer
