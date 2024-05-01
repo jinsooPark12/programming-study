8b31978e14218fb18011c4c54ed3ef724d20dd71
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-05-02 00:52:49 +0900

    [level 3] Title: 스티커 모으기(2), Time: 74.33 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -0,0 +1,21 @@
+def solution(sticker):
+    index = len(sticker)
+    dp1 = [sticker[i] for i in range(0, index-1)]
+    dp2 = [sticker[j] for j in range(1, index)]
+    
+    if index <= 2:
+        return max(sticker)
+
+    for i in range(1, index-1):
+        if i == 1:
+            dp1[i] = max(dp1[i-1], dp1[i])
+        else:
+            dp1[i] = max(dp1[i-1], dp1[i-2] + dp1[i])
+
+    for j in range(1, index-1):
+        if j == 1:
+            dp2[j] = max(dp2[j-1], dp2[j])
+        else:
+            dp2[j] = max(dp2[j-1], dp2[j-2] + dp2[j])
+
+    return max(dp1[index-2], dp2[index-2])
