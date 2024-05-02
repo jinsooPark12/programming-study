fc1baf399895ef96434ce6774ab8ddfcf682d656
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-05-02 13:20:43 +0900

    [level 3] Title: 스티커 모으기(2), Time: 67.15 ms, Memory: 16 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,11 +1,14 @@
 def solution(sticker):
     index = len(sticker)
+    # 첫번째 스티커가 포함된 경우와 포함되지 않은 경우
     dp1 = [sticker[i] for i in range(0, index-1)]
     dp2 = [sticker[j] for j in range(1, index)]
-
+    
+    # 스티커가 2개 이하인 경우
     if index <= 2:
         return max(sticker)
 
+    # i번째 스티커를 때는 경우와 때지 않는 경우 비교
     for i in range(1, index-1):
         if i == 1:
             dp1[i] = max(dp1[i-1], dp1[i])
@@ -14,4 +17,5 @@ def solution(sticker):
             dp1[i] = max(dp1[i-1], dp1[i-2] + dp1[i])
             dp2[i] = max(dp2[i-1], dp2[i-2] + dp2[i])
 
+    # dp연산이 끝난 후 첫번째 스티커가 포함된 경우와 포함되지 않은 경우 비교
     return max(dp1[index-2], dp2[index-2])
