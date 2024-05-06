e578f1602b184882fd3cba3e6131c777ee6c03ba
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-05-06 18:17:57 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -0,0 +1,13 @@
+def solution(skill, skill_trees):
+    answer = 0
+    for i in skill_trees:
+        user_skill = ""
+        k = 0
+        for j in i:
+            if j in skill:
+                user_skill += j
+        if user_skill == "":
+            answer += 1
+        elif user_skill == skill[:len(user_skill)]:
+            answer += 1
+    return answer
