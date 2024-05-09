220a12e3f116dca57b61721cb5fe5ec86c38c470
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-05-09 15:48:32 +0900

    [level 2] Title: 구명보트, Time: 8.03 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,12 @@
+def solution(people, limit):
+    answer = 0
+    r = len(people)-1
+    l = 0
+    people.sort()
+    while l <= r:
+        if people[l] + people[r] <= limit:
+            l += 1
+        r -= 1
+        answer += 1
+
+    return answer
