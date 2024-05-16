668d32ec7040d3add70f82979e74db2852d31ac0
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-05-16 15:31:05 +0900

    [level 2] Title: 양궁대회, Time: 7.72 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -2,16 +2,17 @@ from itertools import product
 def solution(n, info):
     info.reverse()
     ans = [-1]
-    maxd = 0
-    for wl in product((True, False), repeat = 11):
-        s = sum(info[i]+1 for i in range(11) if wl[i])
+    maxScore = 0
+    for TF in product((True, False), repeat = 11):          # 각각의 점수에서 이긴 경우와 진 경우를 전부 계산
+        s = sum(info[i]+1 for i in range(11) if TF[i])      # 각 경우의 화살 개수 계산
         if s <= n:
-            apeach = sum(i for i in range(11) if not wl[i] and info[i])
-            ryan = sum(i for i in range(11) if wl[i])
-            d = ryan - apeach
-            if d > maxd:
-                maxd = d
-                ans = [info[i]+1 if wl[i] else 0 for i in range(11)]
+            # apeach와 ryan 점수 계산
+            apeach = sum(i for i in range(11) if not TF[i] and info[i])
+            ryan = sum(i for i in range(11) if TF[i])
+            score = ryan - apeach
+            if score > maxScore:
+                maxScore = score
+                ans = [info[i]+1 if TF[i] else 0 for i in range(11)]
                 ans[0] += n-s
     ans.reverse()
     return ans
