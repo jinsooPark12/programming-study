5a2931ccece6dccc037f6e83b161309a710fe27d
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-06-06 14:14:56 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 39.72 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,7 +1,10 @@
+# 두 큐의 원소의 합을 같게 -> queue1의 원소의 합 = (모든 원소의 합 // 2) 이면 성립
+
 from collections import deque
 
 def solution(queue1, queue2):
     answer = 0
+    # sum(큐)를 반복문에 넣으면 시간초과
     sum_queue1 = sum(queue1)
     sum_queue2 = sum(queue2)
     total = sum_queue1 + sum_queue2
@@ -9,7 +12,8 @@ def solution(queue1, queue2):
         total = total // 2
     else: 
         return -1
-    n2 = len(queue1)*4
+    # 두 큐의 최대 교환 횟수
+    n2 = len(queue1)*3
     queue1 = deque(queue1)
     queue2 = deque(queue2)
     while 1:
@@ -17,11 +21,13 @@ def solution(queue1, queue2):
             return answer
         if answer >= n2:
             return -1
+        # queue1의 합 > (모든 원소의 합 // 2)
         if sum_queue1 > total:
             num = queue1.popleft()
             queue2.append(num)
             sum_queue1 -= num
             answer += 1
+        # queue1의 합 < (모든 원소의 합 // 2)
         if sum_queue1 < total:
             num = queue2.popleft()
             queue1.append(num)
