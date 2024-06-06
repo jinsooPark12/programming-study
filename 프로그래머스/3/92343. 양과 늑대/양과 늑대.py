37f1c370390b796fc2ae73cb38472dbd4dd95c93
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-06-06 15:06:50 +0900

    [level 3] Title: 양과 늑대, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -1,3 +1,5 @@
+# 모든 노드의 경우의 수를 볼 수 있게 dfs와 백트레킹을 사용
+
 def solution(info, edges):
     visited = [0] * len(info)
     answer = []
@@ -11,10 +13,13 @@ def solution(info, edges):
         for current, next in edges:
             if visited[current] and not visited[next]:
                 visited[next] = 1
+                # 다음 노드가 양일때
                 if info[next] == 0:
                     dfs(sheep+1, wolf)
+                # 다음 노드가 늑대일때
                 else:
                     dfs(sheep, wolf+1)
+                # 백트레킹
                 visited[next] = 0
     visited[0] = 1
     dfs(1, 0)
