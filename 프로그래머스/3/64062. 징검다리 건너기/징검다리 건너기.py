2f1e031351298ac23ff534b88ec31d8bfab74e7e
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-06-06 14:31:54 +0900

    [level 3] Title: 징검다리 건너기, Time: 193.61 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,9 +1,14 @@
+# 단순히 반복문만을 사용한 구현 -> 당연히 효율성 0
+# 효율성 통과를 위해 이분 탐색 사용
+
 def solution(stones, k):
     answer = 0
+    # 탐색의 범위 설정(한 디딤돌을 건널 수 있는 최대, 최소 사람 수)
     left, right = 1, max(stones)
     while left <= right:
         mid = (left + right) // 2
         check = 0
+        # mid 명의 사람들이 디딤돌을 건널 때 한번에 건너 뛰어야 하는 디딤돌의 수 계산
         for stone in stones:
             if (stone - mid) <= 0:
                 check += 1
