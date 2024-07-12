a88bd75f98904e655d87900ea5f4e31a9855f2b5
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-07-12 15:29:56 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,8 @@
+import math
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    result = math.factorial(M) // (math.factorial(N) * math.factorial(M-N))
+    print(result)
