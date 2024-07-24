42361758e90a749b7c6cac78d2e77f6dc21bd02b
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-07-24 17:31:24 +0900

    [Silver III] Title: 피보나치 함수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,7 +1,9 @@
+import sys
+input = sys.stdin.readline
 T = int(input())
 for _ in range(T):
     n = int(input())
-    a,b = 1,0
+    a,b = 1,0               # 0과 1이 호출된 횟수
     for i in range(n):
-        a, b = b, a+b
+        a, b = b, a+b       # 0은 1이 호출된 횟수만큼, 1은 0과 1이 호출된 합만큼
     print(a,b)
