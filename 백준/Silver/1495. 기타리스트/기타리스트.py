d58aaafee076530b1ebf7558a9075300f76ff732
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-07-31 16:11:55 +0900

    [Silver I] Title: 기타리스트, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,20 @@
+n, s, m = map(int,input().split())
+arr = list(map(int,input().split()))
+
+dp = [[0] * (m+1) for _ in range(n+1)]
+dp[0][s] = 1
+
+for i in range(n):
+    for j in range(m+1):
+        if dp[i][j] == 1:
+            if j+arr[i] <= m:
+                dp[i+1][j+arr[i]] = 1
+            if j-arr[i] >= 0:
+                dp[i+1][j-arr[i]] = 1
+      
+ans = -1
+for i in range(m, -1, -1):
+    if dp[n][i] == 1:
+        ans = i
+        break
+print(ans)
